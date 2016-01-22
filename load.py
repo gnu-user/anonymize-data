e4c8c6de86ea0e807cb743f8add2e1ce2910bf4f
gnu_user <gsoc.student@gmail.com>
2016-01-21 22:21:51 -0500

    Updated load script

--- load.py
+++ load.py
@@ -4,8 +4,8 @@ import glob
 import psycopg2 as pg
 
 if __name__ == '__main__':
-    with pg.connect('dbname=daniel') as conn:
+    with pg.connect('dbname=anon_data') as conn:
         with conn.cursor() as cur:
-            for i in glob.glob('*.json'):
+            for i in glob.glob('data/*.json'):
                 cur.execute('INSERT INTO raw_data (data) VALUES (%s)',
                             (open(i).read(),))
